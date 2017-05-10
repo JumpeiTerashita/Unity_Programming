ba7e9a584d8dbe1db328ec451dd0a6031149f2ef
JumpeiTerashita <genuineflat@gmail.com>
2017-05-10 14:36:30 +0900

    first commit

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+    [SerializeField]
+    GameObject Cube;
+
+	// Use this for initialization
+	void Start () {
+       
+       
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Debug.Log("i = "+i+ " j = " + j);
+                GameObject InstObj = Instantiate(Cube);
+                InstObj.transform.position = new Vector3(j,-i,0);
+            }
+                
+            
+
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/ImgPicker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ImgPicker : MonoBehaviour {
+
+   
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(GetPicture());
+
+        
+    }
+	
+	
+
+    IEnumerator GetPicture()
+    {
+        WWW request = new WWW("http://ezlog.biz/wp-content/uploads/2015/05/Unity-Logo.png");
+
+        yield return request;
+
+        if (!string.IsNullOrEmpty(request.error))
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            // webサーバから取得した画像をRaw Imagで表示する
+            RawImage rawImage = GetComponent<RawImage>();
+            rawImage.texture = request.textureNonReadable;
+
+            //ピクセルサイズ等倍に
+            //rawImage.SetNativeSize();
+        }
+    
+    }
+}
