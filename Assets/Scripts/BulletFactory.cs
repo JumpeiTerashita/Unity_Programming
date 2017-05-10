61ecfee525606117e18733701a713e8228a1932e
JumpeiTerashita <genuineflat@gmail.com>
2017-05-10 16:57:03 +0900

    20170510 2D start Creation

---
+++ Assets/Scripts/BulletFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFactory : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject Bullet;
+
+    [SerializeField]
+    float _ShootSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject MakeBullet = Instantiate(Bullet);
+
+            MakeBullet.GetComponent<Rigidbody2D>().AddForce(Vector2.up*_ShootSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
